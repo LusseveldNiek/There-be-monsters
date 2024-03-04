9cc6208590fbd8c66b39607e25d34dd057f59b69
LusseveldNiek <97088465@st.deltion.nl>
2024-03-04 16:42:23 +0100

    ice potion

--- Assets/Script/Monsters/MonsterEscape.cs
+++ Assets/Script/Monsters/MonsterEscape.cs
@@ -4,18 +4,30 @@ public class MonsterEscape : MonoBehaviour
 {
     //added speed per frame
     public float escapeSpeed;
+    private float beginEscapeSpeed;
 
+    //distance atm
     private float currentDistance;
 
     //distance until escape
     public float maxDistance;
+
     //minimum distance
     public float minDistance;
+
+    //time frozen
+    public float freezeTime;
+    private float freezeCounter;
+
+    public float freezeSpeed;
+
+    public bool isFrozen;
 
     private void Start()
     {
         //enemy begins with minDistance
         currentDistance = minDistance;
+        beginEscapeSpeed = escapeSpeed;
     }
 
     void Update()
@@ -34,6 +46,23 @@ public class MonsterEscape : MonoBehaviour
         if(currentDistance > maxDistance)
         {
             print("you lost");
+        }
+
+        if(isFrozen)
+        {
+            escapeSpeed = 0;
+            freezeCounter += Time.deltaTime;
+            if(currentDistance > minDistance)
+            {
+                currentDistance -= freezeSpeed * Time.deltaTime;
+            }
+
+            if(freezeCounter > freezeTime)
+            {
+                freezeCounter = 0;
+                isFrozen = false;
+                escapeSpeed = beginEscapeSpeed;
+            }
         }
     }
 }
