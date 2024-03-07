9107cc953bf8b74a6de3e4319003a188a755986d
elmexx04 <max.simon@live.nl>
2024-03-07 17:00:25 +0100

    trophy scripts

---
+++ Assets/Script/Trophy/CheckIfDouble.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CheckIfDouble : MonoBehaviour
+{
+    public static CheckIfDouble instance;
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
