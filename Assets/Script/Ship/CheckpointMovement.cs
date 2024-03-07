b90f6bf6439532392e9d5e009efdff621877c2ff
LusseveldNiek <97088465@st.deltion.nl>
2024-03-07 11:42:57 +0100

    terrain

--- Assets/Script/Ship/CheckpointMovement.cs
+++ Assets/Script/Ship/CheckpointMovement.cs
@@ -18,26 +18,34 @@ public class CheckpointMovement : MonoBehaviour
     {
         Vector3 targetPosition = shipPositions[currentCheckpointIndex].position;
 
-        // Calculate direction vector from boat to checkpoint
+        //calculate direction vector from boat to checkpoint
         Vector3 direction = (ship.position - targetPosition).normalized;
 
-        // Move the checkpoint in a direction towards the boat
+        //move the checkpoint in a direction towards the boat
         Vector3 newPosition = transform.position + direction * speed * Time.deltaTime;
         transform.position = newPosition;
 
         shipRotation.LookAt(targetPosition);
 
 
-
+        //reached checkpoint
         if (Vector3.Distance(ship.position, targetPosition) < 7)
         {
             print("working");
+
+            //make terrain parent of previous checkpoint
             shipPositions[currentCheckpointIndex].parent = checkpointsManager.transform;
+            //switch to next checkpoint
             currentCheckpointIndex = (currentCheckpointIndex + 1) % shipPositions.Length;
+            //remove parent from next checkpoint
             shipPositions[currentCheckpointIndex].parent = null;
+            //terrain will become child of checkpoint
             transform.parent = shipPositions[currentCheckpointIndex].transform;
+            //rotate with checkpoint rotation
             shipPositions[currentCheckpointIndex].rotation = Quaternion.Euler(shipPositions[currentCheckpointIndex].rotation.x, shipPositions[currentCheckpointIndex].GetComponent<CheckpointRotation>().yRotation, shipPositions[currentCheckpointIndex].rotation.z);
             print("newPosition" + currentCheckpointIndex);
+
+            //I NEED TO ROTATE THE TERRAIN BEFORE GOING TO NEXT CHECKPOINT!!!!
         }
     }
 }
