157a3b9e458f0bf93dc6c1f3525b5a279fa050b8
elmexx04 <max.simon@live.nl>
2024-03-10 13:42:25 +0100

    sync animations with kill bar

--- Assets/Script/Animations/KrakenAnimations.cs
+++ Assets/Script/Animations/KrakenAnimations.cs
@@ -10,6 +10,21 @@ public class KrakenAnimations : MonoBehaviour
     public float minTime;
     public float maxTime;
 
+    public float timeWaitBlockR;
+    public float timeWaitBlockRDone;
+    public float timeWaitBlockM;
+    public float timeWaitBlockL;
+    public float timeWaitBuck;
+
+    public GameObject blockRIndicator;
+    public GameObject blockMIndicator;
+    public GameObject blockLIndicator;
+    public GameObject buckIndicator;
+    public GameObject blockR;
+    public GameObject blockM;
+    public GameObject blockL;
+    public GameObject buck;
+
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
@@ -28,16 +43,19 @@ public class KrakenAnimations : MonoBehaviour
         {
             Debug.Log("Atc row 1");
             anim.SetTrigger("AttackRow1");
+            StartCoroutine(BlockR(timeWaitBlockR));
         }
         if (animatie == 2)
         {
             Debug.Log("Atc row 2");
             anim.SetTrigger("AttackRow2");
+            StartCoroutine(BlockM(timeWaitBlockM));
         }
         if (animatie == 3)
         {
             Debug.Log("Atc row 3");
-            anim.SetTrigger("AttackRow1");
+            anim.SetTrigger("AttackRow3");
+            StartCoroutine(BlockL(timeWaitBlockL));
         }
         if (animatie == 4)
         {
@@ -53,11 +71,13 @@ public class KrakenAnimations : MonoBehaviour
         {
             Debug.Log("Atc hor L");
             anim.SetTrigger("AttackHorizontalL");
+            StartCoroutine(Buck(timeWaitBuck));
         }
         if (animatie == 7)
         {
             Debug.Log("Atc hor R");
             anim.SetTrigger("AttackHorizontalR");
+            StartCoroutine(Buck(timeWaitBuck));
         }
     }
     void Update()
@@ -70,5 +90,34 @@ public class KrakenAnimations : MonoBehaviour
         {
             StartCoroutine(WaitForSpawn());
         }
+    }
+    IEnumerator BlockR(float time)
+    {
+        blockRIndicator.SetActive(true);
+        yield return new WaitForSeconds(time);
+        blockRIndicator.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+    }
+    IEnumerator BlockM(float time)
+    {
+        blockMIndicator.SetActive(true);
+        yield return new WaitForSeconds(time);
+        blockMIndicator.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+    }
+    IEnumerator BlockL(float time)
+    {
+        blockLIndicator.SetActive(true);
+        yield return new WaitForSeconds(time);
+        blockLIndicator.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+
+    }
+    IEnumerator Buck(float time)
+    {
+        buckIndicator.SetActive(true);
+        yield return new WaitForSeconds(time);
+        buckIndicator.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
     }
 }
