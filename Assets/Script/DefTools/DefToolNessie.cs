6e68c887e92cdcb017e0a2ce3179a57afb6649bb
elmexx04 <max.simon@live.nl>
2024-03-15 12:36:16 +0100

    nessie animations

--- Assets/Script/DefTools/DefToolNessie.cs
+++ Assets/Script/DefTools/DefToolNessie.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DefToolNessie : MonoBehaviour
 {
+    public TextMeshProUGUI nummerText;
+    public int animationNummer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animationNummer = 1;
+        nummerText.text = "Animation " + animationNummer.ToString() + "/10";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (animationNummer >= 9)
+        {
+            animationNummer = 1;
+            ReloadUI();
+        }
+    }
+    public void NextNummer()
+    {
+        animationNummer++;
+        ReloadUI();
+    }
+    public void ReloadUI()
+    {
+        nummerText.text = "Animation " + animationNummer.ToString() + "/8";
     }
 }
