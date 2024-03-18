64cf008de87d032863485e2c3aa5d21feff5a28e
elmexx04 <max.simon@live.nl>
2024-03-18 18:22:52 +0100

    nessie implementen en player hit change

--- Assets/Script/Player/PlayerHit.cs
+++ Assets/Script/Player/PlayerHit.cs
@@ -5,13 +5,14 @@ public class PlayerHit : MonoBehaviour
     public GameOverSpawner gameOver;
     public GameObject controller;
     public KrakenAnimations krakenAnimations;
-    public GameObject kraken;
+    public GameObject monster;
+    
     private void Start()
     {
         controller = GameObject.Find("GameController");
         gameOver = controller.GetComponent<GameOverSpawner>();
-        kraken = GameObject.Find("Kraken");
-        krakenAnimations = kraken.GetComponent<KrakenAnimations>();
+        monster = GameObject.Find("Kraken");
+        krakenAnimations = monster.GetComponent<KrakenAnimations>();
     }
     private void OnTriggerEnter(Collider other)
     {
