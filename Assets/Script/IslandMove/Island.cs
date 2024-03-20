f25fe3f14f18ceba21a09c71b68fb5dd16fa1d39
elmexx04 <max.simon@live.nl>
2024-03-20 15:26:04 +0100

    emptys en map movement

---
+++ Assets/Script/IslandMove/Island.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Island : MonoBehaviour
+{
+    public Transform bootR;
+    public Transform bootL;
+
+    public Transform land1;
+    public Transform land2;
+    public Transform land3;
+    public Transform land4;
+    public Transform land5;
+
+}
